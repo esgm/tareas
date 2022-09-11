9ca3ba51d64b14d87ee5dae64d60a93d5a2a161d
Omar Arana <oarana@aei.gt>
2022-09-10 18:03:51 -0600

    Creacion de departamentos

---
+++ tareas/gestion_de_tareas/doctype/departamento/departamento.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, aei.gt and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Departamento', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ tareas/gestion_de_tareas/doctype/departamento/departamento.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, aei.gt and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Departamento(Document):
+	pass
